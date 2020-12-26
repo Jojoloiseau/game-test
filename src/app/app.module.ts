4daed4af89ce27983bec7d38e47e8479a451b67c
Jordi <jordisarpoulet@hotmail.com>
2020-12-26 14:45:13 +0100

    commit second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { ComponentTestComponent } from './component-test/component-test.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component-test/component-test.component.ts
+++ src/app/component-test/component-test.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-component-test',
   templateUrl: './component-test.component.html',
-  styleUrls: ['./component-test.component.scss']
+  styleUrls: ['./../app.component.scss']
 })
 export class ComponentTestComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  this.http.get('assets/test.txt', {responseType: 'text'})
+        .subscribe(data => console.log(data));
   }
+
+  
 
 }
